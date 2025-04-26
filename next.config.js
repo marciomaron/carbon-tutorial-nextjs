bd991ead978cb101dd44baa2ed50cf6d5124e53d
marciomaron <156487527+marciomaron@users.noreply.github.com>
2025-04-26 17:21:23 -0300

    feat(tutorial): complete step 1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ['@carbon/react', '@carbon/icons-react'],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        net: false,
+        dns: false,
+        child_process: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+  // Workaround for ModuleParseError: Module parse failed: Unexpected token (1:0)
+  // https://github.com/vercel/next.js/discussions/12522
+  experimental: {
+    // Allow imports from external modules
+    esmExternals: 'loose',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -0,0 +1,76 @@
+import {
+  Header,
+  HeaderContainer,
+  HeaderName,
+  HeaderNavigation,
+  HeaderMenuButton,
+  HeaderMenuItem,
+  HeaderGlobalBar,
+  HeaderGlobalAction,
+  SkipToContent,
+  SideNav,
+  SideNavItems,
+  HeaderSideNavItems,
+} from '@carbon/react';
+
+const TutorialHeader = () => (
+  <HeaderContainer
+    render={({ isSideNavExpanded, onClickSideNavExpand }) => (
+      <Header aria-label="Carbon Tutorial">
+        <SkipToContent />
+        <HeaderMenuButton
+          aria-label="Open menu"
+          onClick={onClickSideNavExpand}
+          isActive={isSideNavExpanded}
+        />
+        <HeaderName href="/" prefix="IBM">
+          Carbon Tutorial
+        </HeaderName>
+        <HeaderNavigation aria-label="Carbon Tutorial">
+          <Link href="/repos" passHref legacyBehavior>
+            <HeaderMenuItem>Repositories</HeaderMenuItem>
+          </Link>
+        </HeaderNavigation>
+        <SideNav
+          aria-label="Side navigation"
+          expanded={isSideNavExpanded}
+          isPersistent={false}
+        >
+          <HeaderSideNavItems>
+            <Link href="/repos" passHref legacyBehavior>
+              <HeaderMenuItem>Repositories</HeaderMenuItem>
+            </Link>
+          </HeaderSideNavItems>
+        </SideNav>
+        <HeaderGlobalBar>
+          <HeaderGlobalAction
+            aria-label="Notifications"
+            tooltipAlignment="center"
+            className="action-icons"
+          >
+            <Notification size={20} />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction
+            aria-label="User Avatar"
+            tooltipAlignment="center"
+            className="action-icons"
+          >
+            <UserAvatar size={20} />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction aria-label="App Switcher" tooltipAlignment="end">
+            <Switcher size={20} />
+          </HeaderGlobalAction>
+        </HeaderGlobalBar>
+      </Header>
+    )}
+  />
+);
+
+export default TutorialHeader;
+
+import { Switcher, Notification, UserAvatar } from '@carbon/icons-react';
+import Link from 'next/link';
+
+<Link href="/" passHref legacyBehavior>
+  <HeaderName prefix="IBM">Carbon Tutorial</HeaderName>
+</Link>;
